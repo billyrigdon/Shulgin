0f20b2d6278cf94d28a44992af5f62a553688e1a
Billy Rigdon <billyrigdoniii@gmail.com>
2022-02-10 06:03:44 -0600

    Added redux state management

--- Frontend/src/Components/App.tsx
+++ Frontend/src/Components/App.tsx
@@ -3,7 +3,37 @@ import Login from "./Login";
 import Home from "./Home";
 import React from "react";
 import { getAuth } from "../Auth/AuthService";
+import { State } from "../Types/ReduxInterface";
+import { ThunkDispatch } from "redux-thunk";
+import { Action } from "redux";
+import { connect, ConnectedProps } from "react-redux";
+import { toggleLoading } from "Redux/store";
 
+//Redux config
+const mapStateToProps = (state: State) => {
+	return {
+		isLoading: state.isLoading,
+	};
+};
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<any, any, Action>) => {
+	return {
+		toggleLoading: (isLoading: boolean) => {
+			dispatch(toggleLoading(isLoading));
+		},
+	};
+};
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+
+export type Props = PropsFromRedux;
+
+//Connect redux state and dispatch to components
+const ConnectedLogin = connect(mapStateToProps, mapDispatchToProps)(Login);
+
+//Redirect to protected route if authenticated
 const RequireAuth = ({ children, redirectTo }) => {
 	const isAuthenticated = getAuth();
 	return isAuthenticated ? children : <Navigate to={redirectTo} />;
@@ -13,7 +43,7 @@ const App = () => {
 	return (
 		<div>
 			<Routes>
-				<Route path="/login" element={<Login />} />
+				<Route path="/login" element={<ConnectedLogin />} />
 				<Route
 					path="/"
 					element={
--- Frontend/src/Components/Login.tsx
+++ Frontend/src/Components/Login.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../Auth/AuthService";
+import { Props } from "./App";
 
-const Login: React.FC<any> = (props: any) => {
+const Login: React.FC<Props> = (props: Props) => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
-	const [loading, setLoading] = useState(false);
 	const navigate = useNavigate();
 
 	const form = useRef(null);
@@ -19,35 +19,38 @@ const Login: React.FC<any> = (props: any) => {
 		setPassword(e.target.value);
 	};
 
+	const setLoading = () => {
+		props.toggleLoading(props.isLoading);
+	};
+
 	const submitLogin = async (e: any) => {
 		e.preventDefault();
 
-		setLoading(true);
+		setLoading();
 
 		const token = await login(email, password);
+
+		setLoading();
 
 		navigate("/", { replace: true });
 	};
 
 	return (
-		<div id="login-container">
-			<p>Hello this is the login page</p>
-			<form onSubmit={submitLogin} ref={form}>
-				<input
-					type="email"
-					name="email"
-					value={email}
-					onChange={handleEmailChange}
-				></input>
-				<input
-					type="password"
-					name="password"
-					value={password}
-					onChange={handlePasswordChange}
-				></input>
-				<button type="submit"></button>
-			</form>
-		</div>
+		<form onSubmit={submitLogin} ref={form}>
+			<input
+				type="email"
+				name="email"
+				value={email}
+				onChange={handleEmailChange}
+			></input>
+			<input
+				type="password"
+				name="password"
+				value={password}
+				onChange={handlePasswordChange}
+			></input>
+			<button type="submit"></button>
+		</form>
 	);
 };
 
