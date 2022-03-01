5487e6e7619fd8816f58fd28bdb2008f6dfc907f
Billy Rigdon <billyrigdoniii@gmail.com>
2022-03-01 09:41:07 -0600

    More styling changes

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
 	declarations: [
 		AppComponent,
@@ -55,6 +56,7 @@ import { MatButtonModule } from '@angular/material/button';
 		MatFormFieldModule,
 		MatInputModule,
 		MatButtonModule,
+		MatCheckboxModule,
 	],
 	providers: [AuthInterceptorProviders],
 	bootstrap: [AppComponent],
