b9991a15d01ecc0e18f362a54efdd3a3f4aff0f2
Billy Rigdon <billyrigdoniii@gmail.com>
2022-03-13 20:18:02 -0500

    Forgot to add goToDrugs method to add drug button

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,5 +8,5 @@ cd ../Backend
 go get
 go build .
 cd ..
-docker build -t billyrigdoniii/shulgin:1.1.1
-docker push billyrigdoniii/shulgin:1.1.1
+docker build -t billyrigdoniii/shulgin:1.1.2
+docker push billyrigdoniii/shulgin:1.1.2
