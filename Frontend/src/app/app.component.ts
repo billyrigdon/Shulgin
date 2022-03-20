6729b69d42374ad246119b7a65e83103d13a3779
Billy Rigdon <billyrigdoniii@gmail.com>
2022-03-20 14:56:58 -0500

    Pages scroll to top on route change now

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable } from 'rxjs';
+import { filter, Observable } from 'rxjs';
 import { StorageService } from './services/storage.service';
 import { AppState } from './store/app.state';
 import { getAddCommentsOpen } from './store/comments/comments.selector';
@@ -24,7 +25,8 @@ export class AppComponent implements OnInit {
 
 	constructor(
 		private storageService: StorageService,
-		private store: Store<AppState>
+		private store: Store<AppState>,
+		private router: Router
 	) {
 		this.isLoading = this.store.select(getLoading);
 		this.isLoggedIn = this.store.select(getAuthState);
@@ -32,11 +34,19 @@ export class AppComponent implements OnInit {
 	}
 
 	ngOnInit(): void {
+		//Scroll to top of page on route changes
+		this.router.events
+			.pipe(filter(event => event instanceof NavigationEnd))
+			.subscribe(() => {
+				document.body.scrollTop = 0;			
+			});
+
 		if (this.storageService.getToken()) {
 			this.store.dispatch(toggleAuth({ status: true }));
 		}
 
 		if (localStorage.getItem('user')) {
+			
 			//Get userId from user stored in local storage
 			let userId = JSON.parse(localStorage.getItem('user') || '').userId;
 
